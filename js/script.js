eb7c5b2417aeaf5717c4230eafe6e4e8c7038018
Tanitych <Tanitych@gmail.com>
2021-08-16 13:47:54 +0600

    first repos

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        autoplay: false,
+        speed: 1000,
+        autoplaySpeed: 800,
+
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
